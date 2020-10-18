6bdb72b365e60710f6b96afa2f154435315558a1
Your Name <you@example.com>
2020-10-18 19:20:51 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let tempArr = [];
+    const objBrackets = Object.fromEntries(bracketsConfig);
+
+    for (let char of str) {
+        if (tempArr[tempArr.length - 1] === char) {
+            tempArr.pop();
+        } else if (objBrackets[char]) {
+            tempArr.push(objBrackets[char]);
+        } else {
+            return false;
+        }
+    }
+    return tempArr.length === 0;
 }
